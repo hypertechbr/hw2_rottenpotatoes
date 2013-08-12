5b50edff09ed41edd85311690eefa1c8ce6bad0c
hypertechbr <admin@hypertechbrasil.com>
2013-08-11 20:10:16 -0700

    hw_2-b Saasbook

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,11 +7,35 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
-  end
+    
+    sort = params[:sort]
+    @sort = sort
+    if (params[:sort])
+      session[:sort] = sort
+    end
 
-  def new
-    # default: render 'new' template
+    @filter_ratings = []
+    @filter_ratings_hash = {}
+    if (params[:ratings])# || session[:ratings])
+       #if (!params[:ratings])
+       # params[:ratings] = session[:ratings]
+       #end
+
+       params[:ratings].each_key { |keyr|
+         @filter_ratings.push(keyr)
+         @filter_ratings_hash[keyr] = keyr
+       }
+       @movies = Movie.find(:all, :order=>sort, :conditions => {:rating => @filter_ratings})
+       session[:ratings] = @filter_ratings_hash
+    else
+       if (session[:ratings])
+         flash.keep
+         redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
+       end
+       @movies = Movie.all(:order=>sort)
+    end
+
+    @all_ratings = ['G','PG','PG-13','R']
   end
 
   def create
